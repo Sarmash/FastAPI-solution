185876965fded31d9e98234d9fd6aa62e9867ad9
Nikolay <Sarmash777@yandex.ru>
2022-09-27 11:17:37 +0300

    Добавлены изменения в файл api/v1/films

--- fastapi-solution/src/api/v1/films.py
+++ fastapi-solution/src/api/v1/films.py
@@ -46,8 +46,8 @@ async def film_details(
 @router.get("/")
 async def related_films(
     sort: str = Query(default="-imdb_rating"),
-    page_size: int = Query(default=50, gt=1),
-    page_number: int = Query(default=1, gt=1),
+    page_size: int = Query(default=50, gt=0),
+    page_number: int = Query(default=1, gt=0),
     film_service: FilmService = Depends(get_film_service),
 ):
     films = await film_service._get_related_films(sort)
