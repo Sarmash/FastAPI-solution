7ab7fdeb816ce0ef821138d9ee5771c8f47c1984
wood <amper.sil.tok@gmail.com>
2022-09-28 09:13:06 +0300

    оптимизирована пагинация при запросе списка жанров

--- fastapi-solution/src/services/genre.py
+++ fastapi-solution/src/services/genre.py
@@ -3,24 +3,40 @@ from functools import lru_cache
 from aioredis import Redis
 from db.elastic import get_elastic
 from db.redis import get_redis
-from elasticsearch import AsyncElasticsearch
+from elasticsearch import AsyncElasticsearch, helpers
 from fastapi import Depends
 from models.genre import Genre
 from services.service_base import Service
 
 
 class GenreService(Service):
+    """Сервис обработки запросов связанных с жанрами"""
 
-    async def get_genres(self, page: int, page_size: int) -> list[Genre]:
-        """Запрос к elasticsearch на получение списка жанров постранично"""
+    INDEX = 'genres'
 
-        start = 0
-        async for hits in self.scroll('genres', '2m', page_size):
-            genres = [Genre(id=x['_source']['id'],
-                            genre_name=x['_source']['genre']) for x in hits]
-            start += 1
-            if start >= page:
-                return genres
+    async def get_genres(self, page_size: int, page_query: int) -> list[Genre]:
+        """Запрос к elasticsearch на получение списка жанров по заданной странице"""
+
+        if page_query <= 0:
+            page_query = 1
+
+        page = []
+        item_counter = 0
+
+        async for doc in helpers.async_scan(
+                client=self.elastic,
+                index=self.INDEX,
+        ):
+            item_counter += 1
+            if item_counter >= page_query * page_size - page_size:
+                page.append(Genre(id=doc['_source']['id'], genre_name=doc['_source']['genre']))
+                if len(page) == page_size:
+                    return page
+
+        if len(page) > 0:
+            return page
+
+        return page
 
 
 @lru_cache()
--- fastapi-solution/src/services/service_base.py
+++ fastapi-solution/src/services/service_base.py
@@ -1,24 +1,10 @@
 from aioredis import Redis
 from elasticsearch import AsyncElasticsearch
-from typing import Generator
 
 
 class Service:
+    """Родительский класс для сервисов"""
+
     def __init__(self, redis: Redis, elastic: AsyncElasticsearch):
         self.redis = redis
         self.elastic = elastic
-
-    async def scroll(self, index: str, scroll: str, size: int, **kw) -> Generator:
-        """Генератор для запросов к elasticsearch с помощью scroll
-        для возвращения результата постранично"""
-
-        page = await self.elastic.search(index=index, scroll=scroll,
-                                         size=size, **kw)
-        scroll_id = page['_scroll_id']
-        hits = page['hits']['hits']
-
-        while len(hits):
-            yield hits
-            page = await self.elastic.scroll(scroll_id=scroll_id, scroll=scroll)
-            scroll_id = page['_scroll_id']
-            hits = page['hits']['hits']
