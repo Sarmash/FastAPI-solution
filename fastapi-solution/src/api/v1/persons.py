40f4a24b90a3ab8a11998c2deb6c06c5b3546032
Alexey Ostanin <verb11@list.ru>
2022-10-01 17:25:59 +0300

    отредактирована функция вывода одного пользователя

--- fastapi-solution/src/api/v1/persons.py
+++ fastapi-solution/src/api/v1/persons.py
@@ -27,15 +27,14 @@ async def person_list(
 
 
 @router.get('/search/')
-@cache
-async def person_serach(
-        request: Request,
+async def person_info(
         query: str,
+        role: str = Query(default='actors'),
         service: PersonService = Depends(get_person_service),
         page: int = Query(default=1, gt=0),
         page_size: int = Query(default=50, gt=0)
 ) -> list[Person]:
-    list_person = await service.search_person(query, page_size, page)
+    list_person = await service.search_person(query, role, page_size, page)
     if not list_person:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
@@ -44,25 +43,17 @@ async def person_serach(
     return list_person
 
 
-@router.get('/{person_id}', response_model=Person)
+@router.get('/{person_id}/')
 @cache
 async def person_details(request: Request, person_id: str,
+                         role: str = Query(default='actors'),
                          service: PersonService = Depends(
                              get_person_service
                          )) -> list[Person]:
-    person = await service.get_person_detail(person_id)
+    person = await service.get_person_detail(person_id, role)
     if not person:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
             detail='person not found'
         )
     return person
-
-
-@router.get('/info/')
-async def person_info(
-        role: str = Query(default='actors'),
-        service: PersonService = Depends(get_person_service)
-):
-    persons = await service.get_person_info(role)
-    return persons
--- fastapi-solution/src/services/person.py
+++ fastapi-solution/src/services/person.py
@@ -38,7 +38,15 @@ class PersonService(Service):
         except NotFoundError:
             return
 
-    async def get_person_detail(self, person_id: str) -> Optional[Person]:
+    async def get_person_detail(self, person_id: str,
+                                role: str) -> Optional[Person]:
+        film_list = []
+        async for doc in helpers.async_scan(
+                client=self.elastic,
+                query={"_source": {"includes": ["title", role]}},
+                index="movies",
+        ):
+            film_list.append(doc["_source"])
         try:
             person = await self.elastic.get(
                 self.index_person,
@@ -46,55 +54,47 @@ class PersonService(Service):
             )
         except NotFoundError:
             return
-        return Person(
+        person = PersonOut(
             id=person['_source']['id'],
-            full_name=person['_source']['full_name']
+            full_name=person['_source']['full_name'],
+            role=role,
+            film_ids=[]
         )
+        for film in film_list:
+            for actor in film['actors']:
+                if actor['id'] == person.id:
+                    person.film_ids.append(film['title'])
+                    break
+        return person
 
-    async def search_person(self,
-                            query: str,
-                            page_size: int,
+    async def search_person(self, query: str, role: str, page_size: int,
                             page_query: int) -> list:
-        person_list = []
+        film_list = []
+        persons = []
         item_counter = 0
         async for doc in helpers.async_scan(
                 client=self.elastic,
-                index=self.index_person,
-                query={"query": {"match": {"full_name": query}}},
-        ):
-            item_counter += 1
-            if item_counter >= page_query * page_size - page_size:
-                person_list.append(
-                    Person(
-                        id=doc['_source']['id'],
-                        full_name=doc['_source']['full_name'],
-                    )
-                )
-                if len(person_list) == page_size:
-                    return person_list
-        return person_list
-
-    async def get_person_info(self, role: str) -> list:
-        film_list = []
-        persons = []
-        async for doc in helpers.async_scan(
-                client=self.elastic,
                 query={"_source": {"includes": ["title", role]}},
-                index="movies",
+                index=self.index_films,
         ):
             film_list.append(doc["_source"])
         async for doc in helpers.async_scan(
                 client=self.elastic,
                 index=self.index_person,
+                query={"query": {"match": {"full_name": query}}}
         ):
-            persons.append(PersonOut(**doc["_source"], role=role, film_ids=[]))
-        for film in film_list:
-            for actor in film['actors']:
-                for person in persons:
-                    if actor['id'] == person.id:
-                        person.film_ids.append(film['title'])
-                        break
-        return persons
+            item_counter += 1
+            if item_counter >= page_query * page_size - page_size:
+                persons.append(PersonOut(**doc["_source"], role=role, film_ids=[]))
+                for film in film_list:
+                    for actor in film['actors']:
+                        for person in persons:
+                            if actor['id'] == person.id:
+                                person.film_ids.append(film['title'])
+                                break
+                if len(persons) == page_size:
+                    return persons
+            return persons
 
 
 @lru_cache()
