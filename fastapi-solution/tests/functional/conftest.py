77cbb4b7b104925809b3d735f680521ed516a730
wood <amper.sil.tok@gmail.com>
2022-10-06 14:10:40 +0300

    добавлен тест списка жанров

---
+++ fastapi-solution/tests/functional/conftest.py
@@ -0,0 +1,26 @@
+from pytest import fixture
+from aiohttp import ClientSession
+from elasticsearch import AsyncElasticsearch
+from aioredis import create_redis_pool
+
+
+@fixture(scope='session')
+async def http_client():
+    session = ClientSession()
+    yield session
+    await session.close()
+
+
+@fixture(scope="session")
+async def elastic_client():
+    client = AsyncElasticsearch(hosts=['http://elasticsearch:9200'])
+    yield client
+    await client.close()
+
+
+@fixture(scope="session")
+async def redis_client():
+    client = await create_redis_pool(
+        ('redis', 6379)
+    )
+    yield client
