f2598223a934ee63898eb68f33c9e0551c5e7bfa
Nikolay <Sarmash777@yandex.ru>
2022-10-06 09:53:38 +0300

    Добавлены запуск отдельных контейнеров для тестов

--- fastapi-solution/tests/functional/utils/wait_for_es.py
+++ fastapi-solution/tests/functional/utils/wait_for_es.py
@@ -3,10 +3,8 @@ import time
 from elasticsearch import Elasticsearch
 
 if __name__ == "__main__":
-    es_client = Elasticsearch(
-        hosts="http://localhost:9200", validate_cert=False, use_ssl=False
-    )
+    es_client = Elasticsearch(hosts="http://elasticsearch:9200/")
     while True:
         if es_client.ping():
             break
-        time.sleep(1)
+        time.sleep(5)
