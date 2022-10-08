b162624567dbf4a28a67a0d1eed5be898c3feaec
Nikolay <Sarmash777@yandex.ru>
2022-10-08 12:53:24 +0300

    Добавлены тесты к фильмам

--- fastapi-solution/tests/functional/src/data.py
+++ fastapi-solution/tests/functional/src/data.py
@@ -3,18 +3,65 @@ import uuid
 es_data_films = [
     {
         "id": str(uuid.uuid4()),
-        "imdb_rating": 8.5,
-        "genre": ["Action", "Sci-Fi"],
         "title": "The Star",
+        "imdb_rating": 8.5,
         "description": "New World",
-        "director": "Stan",
-        "actors_names": ["Ann", "Bob"],
-        "writers_names": ["Ben", "Howard"],
+        "genre": ["Action", "Sci-Fi"],
         "actors": [
             {"id": "111", "name": "Ann"},
             {"id": "222", "name": "Bob"},
         ],
         "writers": [{"id": "333", "name": "Ben"}, {"id": "444", "name": "Howard"}],
+        "director": "Stan",
+        "actors_names": ["Ann", "Bob"],
+        "writers_names": ["Ben", "Howard"],
     }
     for _ in range(60)
+]
+
+data_film_id = [
+    {
+        "id": "c0142274-dc57-4a3a-a8fe-4c0a229c60f8",
+        "title": "The Star",
+        "imdb_rating": 9.0,
+        "description": "New World",
+        "genre": ["Action"],
+        "actors": [
+            {"id": "111", "name": "Ann"},
+            {"id": "222", "name": "Bob"},
+        ],
+        "writers": [{"id": "333", "name": "Ben"}, {"id": "444", "name": "Howard"}],
+        "director": "Stan",
+        "actors_names": ["Ann", "Bob"],
+        "writers_names": ["Ben", "Howard"],
+    },
+    {
+        "id": "c5dc6c27-1c24-4965-8acc-ae7dcd20801c",
+        "title": "The Star II",
+        "imdb_rating": 8.0,
+        "description": "World",
+        "genre": ["Sci-Fi"],
+        "actors": [
+            {"id": "111", "name": "Ann"},
+        ],
+        "writers": [{"id": "333", "name": "Ben"}, {"id": "444", "name": "Howard"}],
+        "director": None,
+        "actors_names": ["Ann"],
+        "writers_names": ["Ben", "Howard"],
+    },
+    {
+        "id": "c0142274-dc57-4a3a-a8fe-4c0a229c60f1",
+        "title": "The Star III",
+        "imdb_rating": 1.0,
+        "description": "New",
+        "genre": ["Action", "Sci-Fi"],
+        "actors": [
+            {"id": "111", "name": "Ann"},
+            {"id": "222", "name": "Bob"},
+        ],
+        "writers": [],
+        "director": "Stan",
+        "actors_names": ["Ann", "Bob"],
+        "writers_names": [],
+    },
 ]
--- fastapi-solution/tests/functional/src/test_search.py
+++ fastapi-solution/tests/functional/src/test_search.py
@@ -1,28 +1,45 @@
 import pytest
 
+from ..settings import test_settings
 from .data import es_data_films
 
 
 @pytest.mark.parametrize(
-    "query_data, expected_answer, es_data_films",
+    "query_data, expected_answer, data, url",
     [
-        ({"query": "The Star"}, {"status": 200, "length": 50}, es_data_films),
-        ({"query": "Mashed potato"}, {"status": 404, "length": 1}, es_data_films),
+        (
+            {"query": "The Star"},
+            {"status": 200, "length": 50},
+            es_data_films,
+            "search/",
+        ),
+        (
+            {"query": "Mashed potato"},
+            {"status": 404, "length": 1},
+            es_data_films,
+            "search/",
+        ),
     ],
 )
 @pytest.mark.asyncio
 async def test_search(
     make_get_request,
-    es_data_films,
+    data,
     es_write_data,
+    es_delete_data,
+    redis_delete_fixture,
     query_data: dict,
     expected_answer: dict,
+    url: str,
 ):
-
-    await es_write_data(es_data_films)
-
-    response = await make_get_request("films/search/", query_data)
+    url_address = f"{test_settings.service_url}{test_settings.movies_endpoint}{url}"
+    await es_write_data(data)
+    response = await make_get_request(url_address, query_data)
+    response_address = str(response.url)
+    print(response_address)
     body = await response.json()
+    await es_delete_data(test_settings.movies_index)
+    await redis_delete_fixture(response_address)
 
     assert response.status == expected_answer["status"]
     assert len(body) == expected_answer["length"]
